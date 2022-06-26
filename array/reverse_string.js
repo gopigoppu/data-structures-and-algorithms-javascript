8aa4363d9da016b13cef717d33383c64b027757d
Gopi M <gopi.learner93@gmail.com>
2022-06-26 21:40:01 +0530

    reverse string using inbuilt method

--- array/reverse_string.js
+++ array/reverse_string.js
@@ -1,3 +1,4 @@
+// Array backward push
 function reverse(str){
     if(!str || typeof str != 'string' || str.length < 2 ) return str;
 
@@ -9,5 +10,17 @@ function reverse(str){
     return backwards.join('');
 }
 
+// Array reverse() method usage
+function reverse2(str){
+    console.log(str.split(''));
+    return str.split('').reverse().join('');
+}
+
+// spread operator Array reverse()
+// spread operator on a string seems to be the same as using .split('').
+const reverse3 = str => [...str].reverse().join('');
+
 
 reverse('welcome to js diet');
+reverse2('welcome to js diet');
+reverse3('welcome to js diet');
